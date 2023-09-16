39b051ff2920d0dd4fb12520578f6f8f4cce7c63
木村　絵里沙 <erisakimura@gmail.com>
2023-09-16 12:02:57 +0900

    AccountCardの通貨設定

--- src/components/Summary/Card/AccountCard/AccountCard.jsx
+++ src/components/Summary/Card/AccountCard/AccountCard.jsx
@@ -6,37 +6,48 @@ import { UserContext } from '../../../../contexts/UserContext';
 
 const AccountCard = () => {
     const [currentBalance, setCurrentBalance] = useState("---");
+    const [currency, setCurrency] = useState('yen');
     const { user, loading } = useContext(UserContext);
 
     useEffect(() => {
-        if (loading || !user) return; 
-    
+        if (loading || !user) return;
+
         const settingsRef = doc(db, 'setting', user.uid);
-    
-        // onSnapshotを使用してaccountValueの変更をリアルタイムで監視
+
         const unsubscribe = onSnapshot(settingsRef, (docSnapshot) => {
             if (docSnapshot.exists()) {
                 const data = docSnapshot.data();
                 if (data.accountValue) {
-                    // 文字列を数値に変換
                     setCurrentBalance(parseFloat(data.accountValue));
                 } else {
                     setCurrentBalance("---");
                 }
+
+                if (data.currency) {
+                    setCurrency(data.currency);
+                }
             }
         });
-    
-        // クリーンアップ時に監視を停止
+
         return () => unsubscribe();
     }, [user, loading]);
     
+    // 通貨の記号を取得するためのヘルパー関数
+    const getCurrencySymbol = () => {
+        switch(currency) {
+            case 'dollor': return '$';
+            case 'yen': return '¥';
+            case 'euro': return '€';
+            default: return '';
+        }
+    };
 
     return (
         <div className='chartCard chartCard--sm'>
             <div className="chartCard__head">ACCOUNT</div>
             <div className="chartCard__desc">
-                <div className="chartCard__total">¥{currentBalance}</div>
-                <span className='chartCard__sub'>+12%</span> {/* この部分もFirestoreのデータに基づいて動的に変更することができます */}
+                <div className="chartCard__total">{getCurrencySymbol()}{currentBalance}</div>
+                <span className='chartCard__sub'>+12%</span>
             </div>
             <div className="chart">
                 <AccountChart currentBalance={currentBalance !== '---' ? currentBalance : undefined} setCurrentBalance={setCurrentBalance} onBalanceChange={setCurrentBalance} />
